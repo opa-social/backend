b77fcf249258d26c60d6c9063ae71f15a59e5882
MrFlynn <nick@pleatsikas.me>
2020-03-02 16:37:09 -0800

    Added initialization for Realtime Database.

--- internal/firebase/firebase.go
+++ internal/firebase/firebase.go
@@ -6,14 +6,17 @@ import (
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
+	"firebase.google.com/go/db"
 )
 
 // Controller contains app and client instances for Firebase related requests.
 type Controller struct {
 	// Instance is the instance of the Firebase app.
 	Instance *firebase.App
-	// Client is the authentication instances used by the controller.
+	// Client is the authentication instance used by the controller.
 	Client *auth.Client
+	// Database is the database instance used by the controller.
+	Database *db.Client
 }
 
 // New creates a new Firebase controller. Requires that GOOGLE_APPLICATION_CREDENTIALS
@@ -26,11 +29,17 @@ func New() Controller {
 
 	client, err := app.Auth(context.Background())
 	if err != nil {
-		log.Fatal("Could not authenticate with Firebase API")
+		log.Fatal("Could not authenticate with Firebase auth")
+	}
+
+	db, err := app.Database(context.Background())
+	if err != nil {
+		log.Fatal("Could not authenticate with Firebase database")
 	}
 
 	return Controller{
 		Instance: app,
 		Client:   client,
+		Database: db,
 	}
 }
