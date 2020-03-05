82e21039eaf7c43af2f12fb9826384fbb4b436bf
MrFlynn <nick@pleatsikas.me>
2020-03-04 20:12:14 -0800

    Fixed a minor bug in the middleware.
    Turns out spaces are NOT separated in each header field as indicated by
    the docs.

--- internal/firebase/auth.go
+++ internal/firebase/auth.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // AuthMiddleware creates an HTTP authentication middleware handler for authenticating
 // HTTP requests against Firebase authentication.
 func (c *Controller) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		authHeader := r.Header.Values("Authorization")
+		authHeader := strings.Split(r.Header.Get("Authorization"), " ")
+
 		if len(authHeader) < 2 {
 			http.Error(w, "No token provided", http.StatusBadRequest)
 			log.Printf("Client at %s sent empty token or invalid header", r.RemoteAddr)
@@ -24,7 +26,7 @@ func (c *Controller) AuthMiddleware(next http.Handler) http.Handler {
 		_, err := c.Client.VerifyIDToken(context.Background(), authHeader[1])
 		if err != nil {
 			http.Error(w, "Invalid token!", http.StatusUnauthorized)
-			log.Printf("Failed to authenticate user from %s", r.RemoteAddr)
+			log.Printf("Failed to authenticate user from %s with error \"%s\"", r.RemoteAddr, err)
 
 			return
 		}
